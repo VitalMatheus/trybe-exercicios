16fe8015bca948e38856667f70633a7d07d3023f
Vital <matheusvitalds@gmail.com>
2021-11-29 15:47:06 -0300

    saindo de intro a js pra iniciar laços

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/questao1.js
@@ -0,0 +1,8 @@
+let a = 6;
+let b = 2;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/questao2.js
@@ -0,0 +1,9 @@
+let a =10;
+let b = 15;
+
+if (a>b){
+    console.log("o primeiro é maior")
+}
+else{
+    console.log("o segundo é maior")
+}
