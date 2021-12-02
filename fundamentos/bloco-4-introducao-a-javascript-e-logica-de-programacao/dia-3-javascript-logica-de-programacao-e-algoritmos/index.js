df1e1b63b8475599015106159fa50d18dff21bc5
Vital <matheusvitalds@gmail.com>
2021-12-01 21:00:06 -0300

    adidiona dia 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/index.js
@@ -0,0 +1,22 @@
+//let n = 5;
+//let aux = "";
+//let estrela = "*"
+
+//for (let linha=0; linha<n; linha +=1){
+//    aux = aux + estrela;
+//}
+//    for (let linha=0; linha<n; linha +=1){
+//       console.log(aux);
+//}
+
+
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  inputLine = inputLine + symbol;
+};
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  console.log(inputLine);
+};
