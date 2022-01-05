bb69dba70496b93635e8ef60539a674b9d5861cc
Vital <matheusvitalds@gmail.com>
2022-01-04 22:27:12 -0300

    Adiciona fieldset 1 e 2

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,12 @@
+let states = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará','Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal', 'DF']
+
+let selectState = document.getElementById('uf');
+
+function selectStateBrazil(){
+  for (let i=0; i<states.length; i+=1){
+    let itemList = document.createElement('option');
+    itemList.innerText = states[i];
+    selectState.appendChild(itemList);
+  }
+}
+selectStateBrazil();
