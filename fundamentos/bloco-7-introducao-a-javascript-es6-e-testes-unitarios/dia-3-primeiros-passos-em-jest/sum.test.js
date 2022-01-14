54599188463440c3563c60b9f8ebcdedd124d4e4
Vital <matheusvitalds@gmail.com>
2022-01-14 19:34:06 -0300

    adiciona exercicio 1 do dia 7.3

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -0,0 +1,16 @@
+const sum = require ('./sum.js')
+
+describe('Testa a função sum', () => {
+  it('Testa se o retorno de sum(4, 5) é 9', () => {
+    expect(sum(4,5)).toBe(9);
+  });
+  it ('Testa se o retorno de sum(0, 0) é 0', () => {
+    expect(sum(0,0)).toBe(0);
+  });
+  it('Testa se a função sum lança um erro quando os parâmetros são 4 e cinco', () => {
+    expect(() => sum(4,'5')).toThrowError('');
+  });
+  it('Teste se a mensagem de erro é "parameters must be numbers"', () => {
+    expect(() => sum(4,'5')).toThrowError('parameters must be numbers');
+  })
+});
