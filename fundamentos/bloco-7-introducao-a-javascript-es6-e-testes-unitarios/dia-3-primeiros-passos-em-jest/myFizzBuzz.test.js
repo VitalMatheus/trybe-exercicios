180d7157928385334a3705a703ae552be1c66564
Vital <matheusvitalds@gmail.com>
2022-01-14 21:09:28 -0300

    Adiciona exercicio 3 do dia 7.3

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/myFizzBuzz.test.js
@@ -0,0 +1,19 @@
+const myFizzBuzz = require ('./myFizzBuzz.js');
+
+describe ('Testa a função myFizzBuzz', () => {
+  it('Faz uma chamada com um número divisível por 3 e 5 e verifica se o retorno é o esperado', () => {
+    expect(myFizzBuzz(15)).toBe('fizzbuzz');
+  });
+  it('Faz uma chamada com um número divisível por 3 e verifica se o retorno é o esperado', () => {
+    expect(myFizzBuzz(3)).toBe('fizz');
+  });
+  it('Faz uma chamada com um número divisível por 5 e verifica se o retorno é o esperado', () => {
+    expect(myFizzBuzz(5)).toBe('buzz');
+  });
+  it('Faz uma chamada com um número que não é divisível por 3 ou 5 e verifica se o retorno é o esperado', () => {
+    expect(myFizzBuzz(1)).toBe(1);
+  });
+  it('Faz uma chamada com um parâmetro que não é um número e verifica se o retorno é o esperado', () => {
+    expect(myFizzBuzz('7')).toBe(false);
+  });
+});
